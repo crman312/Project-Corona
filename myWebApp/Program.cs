a3b0f4eb63ea1b8bf0fe45e11a93442224cb6985
Jan-Jaap <jan-jaaptencate@hotmail.com>
2020-10-26 10:30:19 +0100

    database connection

--- myWebApp/Program.cs
+++ myWebApp/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Npgsql;
 
 namespace myWebApp
 {
@@ -14,6 +15,19 @@ namespace myWebApp
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+
+
+            var cs = "Server=localhost;Port=5432;Database=Corona kantoor app;User Id=postgres;Password=Minicooper1";
+
+            using var con = new NpgsqlConnection(cs);
+            con.Open();
+
+            var sql = "SELECT version()";
+
+            using var cmd = new NpgsqlCommand(sql, con);
+
+            var version = cmd.ExecuteScalar().ToString();
+            Console.WriteLine($"PostgreSQL version: {version}");
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
--- myWebApp/Startup.cs
+++ myWebApp/Startup.cs
@@ -23,6 +23,8 @@ namespace myWebApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+
+           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
