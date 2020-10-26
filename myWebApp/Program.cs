2c968736ad4b5783bf05663b6d76e2834c087cac
Jan-Jaap <jan-jaaptencate@hotmail.com>
2020-10-26 11:41:13 +0100

    test

--- myWebApp/Program.cs
+++ myWebApp/Program.cs
@@ -17,16 +17,13 @@ namespace myWebApp
             CreateHostBuilder(args).Build().Run();
 
 
-            var cs = "Host=localhost;Username=postgres;Password=Minicooper1;Database=Corona kantoor app";
+            var cs = "Host=localhost;Username=Corona kantoor app;Password=Minicooper1;Database=Corona kantoor app";
 
             using var con = new NpgsqlConnection(cs);
             con.Open();
 
             using var cmd = new NpgsqlCommand();
             cmd.Connection = con;
-
-            cmd.CommandText = "DROP TABLE IF EXISTS employee";
-            cmd.ExecuteNonQuery();
 
             cmd.CommandText = "INSERT INTO employee(user_id, password, email) VALUES(2,'jewed','ttt@hsd.nl')";
             cmd.ExecuteNonQuery();
