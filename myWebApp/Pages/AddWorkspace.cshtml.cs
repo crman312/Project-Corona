aac7d60df882a80fd8a03b39ad19fe0019a0ba4d
Jan-Jaap <jan-jaaptencate@hotmail.com>
2020-11-16 16:35:35 +0100

    yes

--- myWebApp/Pages/AddWorkspace.cshtml.cs
+++ myWebApp/Pages/AddWorkspace.cshtml.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using myWebApp.Database;
+using myWebApp.Models;
 using Npgsql;
 
 
@@ -13,19 +14,28 @@ namespace myWebApp.Pages
 {
     public class AddWorkspaceModel : PageModel
     {
-        private readonly ILogger<AddWorkspaceModel> _logger;
+        /*private readonly ILogger<AddWorkspaceModel> _logger;
 
         public AddWorkspaceModel(ILogger<AddWorkspaceModel> logger)
         {
             _logger = logger;
-        }
+        }*/
 
+        public string Info { get; set; }
+ 
         public void OnGet()
         {
-
+        }
+ 
+        public void OnPostSubmit(WorkspaceModel workspace)
+        {
+            this.Info = string.Format("Info: {0} {1} {3}", workspace.LocationName, workspace.RoomName, workspace.SquareMeters);
         }
 
 
+
+
+       
   
 
 
@@ -55,29 +65,5 @@ namespace myWebApp.Pages
                     
         }
                     
-    }
-
-    public class AddWorkspaceModel
-    {
-        public string LocationName { get; set; }
-        public string RoomName { get; set; }
-        public string SquareMeters { get; set; }
-
-
-        locationInput = Request.Form["Location"];
-        roomInput = Request.Form["Room"];
-        squaremetersInput = Request.Form["SquareMeters"];
-
-        var cs = "Host=localhost;Username=postgres;Password=admin;Database=Corona kantoor app";
-
-        using var con = new NpgsqlConnection(cs);
-        con.Open();
-
-        using var cmd = new NpgsqlCommand();
-        cmd.Connection = con;
-
-        cmd.CommandText = "INSERT INTO workspaces(location, room, squareMeters) VALUES('Washington', 2, 100)"; //@locationInput, @roomInput, @squaremetersInput
-        cmd.ExecuteNonQuery(); 
-
     }
 }
