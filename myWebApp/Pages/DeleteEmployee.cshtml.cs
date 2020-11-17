779cfea3772a81e6b492a18bb23e16671e5f7144
Jan-Jaap <jan-jaaptencate@hotmail.com>
2020-11-17 19:31:55 +0100

    test showing data

--- myWebApp/Pages/DeleteEmployee.cshtml.cs
+++ myWebApp/Pages/DeleteEmployee.cshtml.cs
@@ -14,6 +14,17 @@ namespace myWebApp.Pages
 {
     public class DeleteEmployeeModel : PageModel
     {
+        private readonly ILogger<DeleteEmployeeModel> _logger;
 
+        public DeleteEmployeeModel(ILogger<DeleteEmployeeModel> logger)
+        {
+            _logger = logger;
+        }
+        
+        public void OnGet()
+        {
+            ViewData["MyNumber"] = 42;
+            ViewData["MyString"] = "Hello World";
+        }
     }
 }
