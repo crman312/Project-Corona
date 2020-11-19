0cbee7b42712778e9aa9e4eba706acc4b89f9912
Darja <dvidelina@gmail.com>
2020-11-19 12:35:01 +0100

    logincopy

--- Project_Corona/Pages/Index.cshtml.cs
+++ Project_Corona/Pages/Index.cshtml.cs
@@ -5,6 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Project_Corona.Database;
+using Project_Corona.Models;
+using Npgsql;
 
 namespace Project_Corona.Pages
 {
@@ -20,6 +23,39 @@ namespace Project_Corona.Pages
         public void OnGet()
         {
 
+        }
+        protected void LoginButton_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("/admin");
+
+        }
+        public void LoginCheck(string email, string password)
+        {
+            try
+			{
+                var cs = "Host=localhost;Username=postgres;Password=admin;Database=Corona kantoor app";
+				using var con = new NpgsqlConnection(cs);
+                con.Open();
+		
+                using var cmd = new NpgsqlCommand();
+                cmd.Connection = con;
+
+                cmd.CommandText = @"SELECT * FROM employee WHERE email = @email AND password = @password";
+
+                string emailInput = Request.Form["email"];
+                string passwordInput = Request.Form["password"];
+
+
+
+                
+
+            }
+            catch (Exception)
+			{
+
+				throw;
+			}
+			
         }
     }
 }
