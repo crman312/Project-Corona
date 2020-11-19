d33c7ed171bdc2366874976931d827977b1f66ee
Jan-Jaap <jan-jaaptencate@hotmail.com>
2020-11-19 18:24:48 +0100

    show employees

--- myWebApp/Pages/DeleteEmployee.cshtml.cs
+++ myWebApp/Pages/DeleteEmployee.cshtml.cs
@@ -23,8 +23,36 @@ namespace myWebApp.Pages
         
         public void OnGet()
         {
+            var ss = ShowEmployees();
             ViewData["MyNumber"] = 42;
-            ViewData["MyString"] = "Hello World";
+            ViewData["MyString"] = ss;
         }
+
+        public List<string> ShowEmployees()
+        {
+            List<string> EmployeeNames = new List<string>();
+
+            var cs = Database.Database.Connector();
+
+            using var con = new NpgsqlConnection(cs);
+            con.Open();
+
+            var sql = "SELECT * FROM employees";
+            using var cmd = new NpgsqlCommand(sql, con);
+
+            NpgsqlDataReader dRead = cmd.ExecuteReader();
+
+            int employeeFunction = 0;
+           
+            while (dRead.Read())
+            {
+                for(int i = 0; i < dRead.FieldCount; i++)
+                {
+                    EmployeeNames.Add(dRead[1].ToString());
+                }
+            }
+            return EmployeeNames;
+        }
+        
     }
 }
