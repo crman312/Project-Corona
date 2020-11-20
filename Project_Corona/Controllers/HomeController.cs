f9957c689e447930f66267df5bc6036ba599f6de
Darja <dvidelina@gmail.com>
2020-11-20 12:15:41 +0100

    commit

--- Project_Corona/Controllers/HomeController.cs
+++ Project_Corona/Controllers/HomeController.cs
@@ -17,36 +17,7 @@ namespace Project_Corona.Controllers
     {
 
 
-        public IActionResult Employeepage()
-        {
-            List<WorkspaceModel> res = PopulateReservations();
-            return View(new SelectList(res, "LocationName"));
-        } 
-
-        private static List<WorkspaceModel> PopulateReservations()
-        {
-            var cs = Database.Database.Connector();
-            List<WorkspaceModel> res = new List<WorkspaceModel>();
-            using var con = new NpgsqlConnection(cs);
-            {
-                string query = "Select location FROM workspaces";
-                using NpgsqlCommand cmd = new NpgsqlCommand(query, con);
-                {
-                    cmd.Connection = con;
-                    con.Open();
-                    using (NpgsqlDataReader dr = cmd.ExecuteReader())
-                    {
-                        while (dr.Read())
-                        {
-                            res.Add(new WorkspaceModel{LocationName = dr["LocationName"].ToString()});
-                        }
-                    }
-                    con.Close();
-                }
-            }
         
-            return res;
-        }
       
     }
     
