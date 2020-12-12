7d71fa6e6a215fe30200c0df1266b383ef8252ea
Darja <dvidelina@gmail.com>
2020-12-12 10:32:32 +0100

    commit

---
+++ myWebApp/MyHub.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace myWebApp.MyHub
+{
+    public class MyHub : Hub
+    {
+
+    }
+}
